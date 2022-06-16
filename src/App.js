ff1129f484467678845b89d0eaf0e8520a584481
hypatiasf <hypatia.starfall@gmail.com>
2022-06-16 19:35:30 +1000

    that's all

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import _ from 'lodash';
 
 function App() {
+  const [source, setSource] = useState('');
+  const [chosen, setChosen] = useState('');
+
+  const buttonOnClickHandler = (event) => {
+    event.preventDefault();
+    const rows = source.split('\n');
+    console.log(rows);
+
+    const result = _.sample(rows);
+    setChosen(result);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{chosen}</h1>
+
+      <form className={'source-form'}>
+        <textarea className={'source-text'} value={source} onChange={(event => {
+          setSource(event.target.value);
+        })}/>
+
+        <button onClick={buttonOnClickHandler}>Black</button>
+      </form>
+
+      <h1>{chosen}</h1>
     </div>
   );
 }
